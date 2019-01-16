eba63dcaa1db92aa3d35246155db44fa6cf9f2db
Solomon Padilla <goryfigment@gmail.com>
2019-01-15 17:28:29 -0700

    Fixed ebay url search.

--- templates/js/result.js
+++ templates/js/result.js
@@ -1,5 +1,7 @@
 require('./../css/general.css');
 require('./../css/result.css');
+require('./../css/header.css');
+require('./../js/header.js');
 require('./../library/tippy/tippy.css');
 require('./../library/fontawesome/fontawesome.js');
 
@@ -172,25 +174,6 @@ $(document).ready(function() {
             e.stopPropagation();
             $('#craigslist-input').css('border', 'solid 2px #ff0000')
         }
-    });
-
-    $(document).on('keydown', '#search-input', function (e) {
-        var $searchInput =  $('#search-input');
-
-        if ($searchInput.val().trim().length > 0 && e.keyCode == 13) {
-            $('#submit').click();
-            $searchInput.prop('disabled', true);
-        }
-    });
-
-    $('form').submit(function(e){
-        if ($('#search-input').val().trim().length < 1) {
-            e.preventDefault();
-        }
-    });
-
-    $(document).on('click', '.search-icon', function () {
-        $('#submit').click();
     });
 });
 
